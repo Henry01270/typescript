48944261977bccc1f577c8e58d01f85eaeda7147
Henry01270 <118987076+Henry01270@users.noreply.github.com>
2025-05-30 13:42:43 +0000

    25_0530_1

---
+++ 2_union_and_intersection/2_intersection_basics.ts
@@ -0,0 +1,34 @@
+/**
+ * Intersection
+ * 
+ * Union이 Or의 개념이라면
+ * Intersection은 And의 개념
+ * 
+ * 선언한 프로퍼티를 추가하면
+ * 추가한 모든 프로퍼티에 해당하는 값을 입력해주어야함
+ */
+ interface Human{
+    name: string;
+    age: number;
+ }
+
+ interface Contacts{
+    phone: string;
+    adress: string;
+ }
+
+ type HumanAndContacts = Human & Contacts;
+
+ let humanAndContacts: HumanAndContacts = {
+    name: '코드팩토리',
+    age: 32,
+    phone: '01012341234',
+    adress: '서울시',
+ };
+ 
+
+ type NumberAndString = number & string;
+ // primitive 타입을 인터섹션을 만들어버리면 never 타입으로 변경된다.
+
+ // let numberAndString: NumberAndString = null;
+ // => 어떤 값을 넣어도 err
